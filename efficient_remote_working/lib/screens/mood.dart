3965d460d5cdaf7472720ba70ea943003e702905
Joost Raemakers <jraemakers@outlook.com>
2024-03-07 11:10:53 +0100

    Update function to handle edge cases

--- efficient_remote_working/lib/screens/mood.dart
+++ efficient_remote_working/lib/screens/mood.dart
@@ -29,197 +29,97 @@ class MoodScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xFF1E1D2A),
       ),
-      body: Container(
-        color: const Color(0xFF1E1D2A),
-        child: Stack(
-          children: [
-            const Positioned(
-              top: 50,
-              left: 16,
-              width: 75,
-              height: 75,
-              child: CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/images/andrea.jpg',
-                ),
-                radius: 24,
-              ),
-            ),
-            const Positioned(
-              top: 175,
-              left: 16,
-              width: 75,
-              height: 75,
-              child: CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/images/justin.jpg',
-                ),
-                radius: 24,
-              ),
-            ),
-            const Positioned(
-              top: 300,
-              left: 16,
-              width: 75,
-              height: 75,
-              child: CircleAvatar(
-                backgroundImage: AssetImage(
-                  'assets/images/stefan.jpg',
-                ),
-                radius: 24,
-              ),
-            ),
-            Positioned(
-              top: 30,
-              left: 100,
-              right: 16,
-              child: Container(
-                height: 110,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF252736),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '😴',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                            'SARAH V.',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'SARAH IS FEELING: LAZY',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    return Container(
+      color: const Color(0xFF1E1D2A),
+      child: Stack(
+        children: [
+          buildPositionedAvatar(top: 50, imageName: 'andrea.jpg'),
+          buildPositionedAvatar(top: 175, imageName: 'justin.jpg'),
+          buildPositionedAvatar(top: 300, imageName: 'stefan.jpg'),
+          buildMoodContainer(
+              top: 30, name: 'SARAH V.', emoji: '😴', mood: 'LAZY'),
+          buildMoodContainer(
+              top: 160, name: 'MIKE S.', emoji: '😃', mood: 'HAPPY'),
+          buildMoodContainer(
+              top: 290, name: 'TOM K.', emoji: '😡', mood: 'ANGRY'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPositionedAvatar(
+      {required double top, required String imageName}) {
+    return Positioned(
+      top: top,
+      left: 16,
+      width: 75,
+      height: 75,
+      child: CircleAvatar(
+        backgroundImage: AssetImage(
+          'assets/images/$imageName',
+        ),
+        radius: 24,
+      ),
+    );
+  }
+
+  Widget buildMoodContainer(
+      {required double top,
+      required String name,
+      required String emoji,
+      required String mood}) {
+    return Positioned(
+      top: top,
+      left: 100,
+      right: 16,
+      child: Container(
+        height: 110,
+        decoration: BoxDecoration(
+          color: const Color(0xFF252736),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    emoji,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 160,
-              left: 100,
-              right: 16,
-              child: Container(
-                height: 110,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF252736),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '😃',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                            'MIKE S.',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'MIKE IS FEELING: HAPPY',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+                  const SizedBox(width: 8),
+                  Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '$name IS FEELING: $mood',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
-            ),
-            Positioned(
-              top: 290,
-              left: 100,
-              right: 16,
-              child: Container(
-                height: 110,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF252736),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: const Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '😡',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                            'TOM K.',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'TOM IS FEELING: ANGRY',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
